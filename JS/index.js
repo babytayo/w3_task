b10a4eefe0019baaff5a6d724c4b3b1cc9e3e823
babytayo <121583836+babytayo@users.noreply.github.com>
2023-04-06 00:15:56 +0100

    added js

---
+++ JS/index.js
@@ -0,0 +1,11 @@
+// window.alert("You're welcome to check Omotayo's week one task.");
+
+//signUp page function
+function signUp() {
+  alert("Successfully submitted");
+}
+
+//login function
+function login() {
+  window.alert("Logged in");
+}
